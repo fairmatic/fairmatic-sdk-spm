55820fd877b60fe05aa5b17d3bf0f0f6c6afd70c
Sagar Dagdu <shags032@gmail.com>
2024-05-06 11:50:39 +0530

    Add `Package.swift` and empty placeholder swift files

---
+++ Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FairmaticSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "FairmaticSDK",
+            targets: ["FairmaticSDKTarget"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/fairmatic/SQLite.swift.git",
+            .upToNextMinor(from: "0.15.4")
+        ),
+        .package(
+            url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git",
+            .upToNextMinor(from: "2.34.2")
+        )
+    ],
+    targets: [
+        .target(
+            name: "FairmaticSDKWrapper",
+            dependencies: [
+                .target(name: "FairmaticSDK"),
+                .product(name: "SQLite", package: "SQLite.swift"),
+                .product(name: "AWSSQS", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCore", package: "aws-sdk-ios-spm")
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("Accelerate"),
+                .linkedLibrary("z"),
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("c++"),
+                .unsafeFlags([
+                    "-ObjC", "-lC++"
+                ])
+            ]
+        ),
+        .binaryTarget(
+            name: "FairmaticSDK",
+            path: "FairmaticSDK.xcframework"
+        ),
+        .target(
+            name: "FairmaticSDKTarget",
+            dependencies: [.target(name: "FairmaticSDKWrapper")]
+        )
+    ]
+)
---
+++ Sources/FairmaticSDKTarget/FairmaticSDKTargetPlaceholder.swift
@@ -0,0 +1,8 @@
+//
+// FairmaticSDKTargetPlaceholder.swift
+//
+// Copyright © 2024 Fairmatic. All rights reserved.
+//
+
+// Empty source file to satisfy SPM. This is a binary package.
+
