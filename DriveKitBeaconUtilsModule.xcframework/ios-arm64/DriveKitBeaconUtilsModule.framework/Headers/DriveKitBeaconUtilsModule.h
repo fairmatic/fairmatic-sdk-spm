a767d145801059c27fc25488780884bfb2eb52f7
Sagar Dagdu <shags032@gmail.com>
2025-04-06 19:12:31 +0530

    Add `xcframework`s for DriveKit

---
+++ DriveKitBeaconUtilsModule.xcframework/ios-arm64/DriveKitBeaconUtilsModule.framework/Headers/DriveKitBeaconUtilsModule.h
@@ -0,0 +1,19 @@
+//
+//  DriveKitBeaconUtilsModule.h
+//  DriveKitBeaconUtilsModule
+//
+//  Created by David Bauduin on 08/06/2022.
+//  Copyright © 2022 DriveQuant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DriveKitBeaconUtilsModule.
+FOUNDATION_EXPORT double DriveKitBeaconUtilsModuleVersionNumber;
+
+//! Project version string for DriveKitBeaconUtilsModule.
+FOUNDATION_EXPORT const unsigned char DriveKitBeaconUtilsModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DriveKitBeaconUtilsModule/PublicHeader.h>
+
+
